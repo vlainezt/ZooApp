ba40bc58e97814ef09cf5e169b3f1b6a0dc535d1
vlainezt <vlainezt07@gmail.com>
2021-03-27 09:15:54 -0600

    agregando gitignore

--- ZooApp/Conexion.cs
+++ ZooApp/Conexion.cs
@@ -10,7 +10,7 @@ namespace ZooApp
         private static SqlConnection conexion()
         {
             SqlConnection conn = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\zooapp\\ZooApp\\Zoo.mdf; Integrated Security = True");
-
+            //SqlConnection conn = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = |DataDirectory|\\Zoo.mdf; Integrated Security = True");
             return conn;
         }
 
