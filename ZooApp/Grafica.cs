6b54e23f0a8750aff33f2cc6570de01a0a9af422
Stanley Cortez <johansson.05@hotmail.com>
2021-05-19 23:34:27 -0500

    Creando funciones para obtener la cantidad de animales estan con habitat y el rating de Especies

---
+++ ZooApp/Grafica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ZooApp
+{
+    public partial class Grafica : Form
+    {
+        ControlAnimals controls = new ControlAnimals();
+        //controls.getRatingHabitat(); Pasale DESC si queres que sea descendiente como string
+        //controls.getRatingEspecie(); Pasale ASC si queres que sea ascendente como string
+
+        public Grafica()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Grafica_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
